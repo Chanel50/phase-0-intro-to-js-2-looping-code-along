89cef381c4fff30694e6939450772d0be086b4f9
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-13 17:41:47 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    let countdown = number;
+  
+    while (countdown >= 0) {
+      console.log(countdown);
+      countdown--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
